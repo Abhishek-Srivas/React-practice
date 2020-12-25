14356e836d462240423eed6b4f0ccb3abd9a451a
Abhishek Srivas <abhiautar123@gmail.com>
2020-12-26 00:18:35 +0530

    added a dynamic  content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>I'm a Person, called from a function</p>
+    return <p>I'm a Person,my age is {Math.floor(Math.random() * 40)}</p>
 };
 
 export default person;
